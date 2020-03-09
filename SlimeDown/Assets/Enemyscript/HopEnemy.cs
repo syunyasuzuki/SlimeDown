cccea807a8db46c59b061d199b62cb7629bc90b8
ogihararyuki <nkcgta.ogihara.ryuki@gmail.com>
2020-03-09 10:03:31 +0900

    script追加、scriptファイル追加

---
+++ SlimeDown/Assets/Enemyscript/HopEnemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HopEnemy : MonoBehaviour
+{
+
+    //private float up = 0;
+    //private float down = 0;
+    private float timer = 0;
+    private bool flag = true;//跳ねる時のフラグ
+    public float x, y;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "wall")
+        {
+            flag = !flag;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1.5)
+        {
+            timer = 0;
+            if (flag == true)
+            {
+                Rigidbody2D rb = this.GetComponent<Rigidbody2D>();  // rigidbodyを取得
+                Vector2 force = new Vector2(x, y);  // 力を設定
+                rb.AddForce(force, ForceMode2D.Impulse);          // 力を加える
+            }
+            if (flag == false)
+            {
+
+                Rigidbody2D rb = this.GetComponent<Rigidbody2D>();  // rigidbodyを取得
+                Vector2 force = new Vector2(-x, y);  // 力を設定
+                rb.AddForce(force, ForceMode2D.Impulse);          // 力を加える
+            }
+        }
+
+
+    }
+}
---
+++ SlimeDown/Assets/Enemyscript/fluffyEnemy.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fluffyEnemy : MonoBehaviour
+{
+    private bool turn, up, down; //ターン,up,downフラグ
+    public float x_up, y_up, x_down, y_down;
+    private float timer = 0; //タイマー
+    public float clock;
+
+    // Use this for initialization
+    void Start()
+    {
+        up = true;
+        down = false;
+        turn = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "wall")
+        {
+            turn = !turn;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+        Vector2 now = rb.position; //敵の座標を取得
+        timer += Time.deltaTime;
+        if (turn == true)
+        {
+            if (up == true)
+            {
+                now += new Vector2(x_up, y_up);  // 前に少しずつ移動するように加算
+                rb.position = now; // 値を設定
+                if (timer > clock)
+                {
+                    up = false;
+                    down = true;
+                    timer = 0;
+                }
+            }
+            if (down == true)
+            {
+                now += new Vector2(x_down, y_down);  // 前に少しずつ移動するように加算
+                rb.position = now; // 値を設定
+                if (timer > clock)
+                {
+                    up = true;
+                    down = false;
+                    timer = 0;
+                }
+            }
+        }
+        if (turn == false)
+        {
+            if (up == true)
+            {
+                now -= new Vector2(x_up, y_up);  // 前に少しずつ移動するように加算
+                rb.position = now; // 値を設定
+                if (timer > clock)
+                {
+                    up = false;
+                    down = true;
+                    timer = 0;
+                }
+            }
+            if (down == true)
+            {
+                now -= new Vector2(x_down, y_down);  // 前に少しずつ移動するように加算
+                rb.position = now; // 値を設定
+                if (timer > clock)
+                {
+                    up = true;
+                    down = false;
+                    timer = 0;
+                }
+            }
+        }
+
+    }
+}
